16481bfebb07c8116a8900845c58fdb68f6be435
craytonjagneaux419 <craytonjagneaux419@gmail.com>
2025-03-07 07:33:16 +0000

    Add 2.rs

---
+++ src/2.rs
@@ -0,0 +1,11 @@
+use std::collections::HashMap;
+
+fn main() {
+    let mut map = HashMap::new();
+    map.insert(1, "hello");
+    map.insert(2, "world");
+
+    for (key, value) in &map {
+        println!("{}: {}", key, value);
+    }
+}
