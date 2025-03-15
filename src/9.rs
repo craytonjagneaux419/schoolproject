7c09a67e824138e3fea759054bb8959720022b2c
craytonjagneaux419 <craytonjagneaux419@gmail.com>
2025-03-15 01:51:16 +0000

    Add 9.rs

---
+++ src/9.rs
@@ -0,0 +1,6 @@
+// A simple calculator program that takes two numbers as input and returns their sum
+fn main() {
+    let num1 = 5;
+    let num2 = 3;
+    println!("The sum of {} and {} is {}", num1, num2, num1 + num2);
+}
